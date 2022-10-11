d5b0aa2330a8e30acb646411fc0ade0295c4cc32
rbsysad <rbsysad@gmail.com>
2022-10-11 15:16:24 -0500

    Add solution to 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    let temp;
+    let sum = 0;
 
+    if (!Number.isFinite(num1) || !Number.isFinite(num2)) {
+        return 'ERROR';
+    }
+
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+
+    if (num1 > num2) {
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
