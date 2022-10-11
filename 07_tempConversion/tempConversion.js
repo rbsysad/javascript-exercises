5b20529dba0ebaac209d46a0dbb8299514275223
rbsysad <rbsysad@gmail.com>
2022-10-11 15:52:53 -0500

    Add solution to 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (givenTemp) {
+    return Math.round((givenTemp - 32) * (5 / 9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (givenTemp) {
+    return Math.round((givenTemp * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof,
 };
