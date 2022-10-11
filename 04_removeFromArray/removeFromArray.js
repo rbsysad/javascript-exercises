b2028c48da8ab09440e4f2cb7f801b6ef32d3239
rbsysad <rbsysad@gmail.com>
2022-10-11 14:45:29 -0500

    Add solution to 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    args.forEach((argument) => {
+        array = array.filter((item) => item !== argument);
+    });
 
+    return array;
 };
 
 // Do not edit below this line
