74072d602af70a80d6a3b18f40f5c47a88fd8447
rbsysad <rbsysad@gmail.com>
2022-10-14 15:54:05 -0500

    Add solution to 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+// Function for calculating age
+const getAge = (birthYear, deathYear) => {
+    if (!deathYear) {
+        deathYear = new Date().getFullYear();
+    }
+    return deathYear - birthYear;
+};
 
+// Use Array.reduce method to fild oldest person
+const findTheOldest = function (personArray) {
+    return personArray.reduce((oldestPerson, currentPerson) => {
+        // Oldest age so far in array of objects
+        const oldestAge = getAge(
+            oldestPerson.yearOfBirth,
+            oldestPerson.yearOfDeath
+        );
+
+        // Current age of element in array of objects
+        const currentAge = getAge(
+            currentPerson.yearOfBirth,
+            currentPerson.yearOfDeath
+        );
+
+        // If oldest is less than current, replace oldest with current
+        return oldestAge < currentAge ? currentPerson : oldestPerson;
+    });
 };
 
 // Do not edit below this line
