3435d4b6e7909e5fd15ee0d3667e1eb16720fa22
rbsysad <rbsysad@gmail.com>
2022-10-14 14:23:32 -0500

    Add solution to 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (element) {
+    // Reject negatives
+    if (element < 0) {
+        return 'OOPS';
+    }
 
+    // Keep track of previous 2 numbers and next number
+    let n1 = 0,
+        n2 = 1,
+        nextNum;
+
+    // Determine element, if string convert to num
+    for (let i = 1; i <= +element; i++) {
+        nextNum = n1 + n2;
+        n1 = n2;
+        n2 = nextNum;
+    }
+
+    return n1;
 };
 
 // Do not edit below this line
