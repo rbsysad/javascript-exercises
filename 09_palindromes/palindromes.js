7e44142a75e7a30c9cffdded5cf126e87ed4a104
rbsysad <rbsysad@gmail.com>
2022-10-14 13:27:33 -0500

    Add solution to 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // Regular expression to filter out non alphanumeric characters
+    re = /[^A-Za-z0-9]/g;
 
+    // Filter re with replace method
+    const lowRegString = string.toLowerCase().replace(re, '');
+
+    // Split string into array, reverse array, join string
+    const reverseString = lowRegString.split('').reverse().join('');
+
+    // Check if equal to non-reversed
+    return lowRegString === reverseString;
 };
 
-// Do not edit below this line
+// // Do not edit below this line
 module.exports = palindromes;
