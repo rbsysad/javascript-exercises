4d65ada1a992ba82268f50275e3d617f88b63ee8
rbsysad <rbsysad@gmail.com>
2022-10-14 14:27:13 -0500

    Add solution to 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (booksArray) {
+    const titleArray = [];
+    booksArray.forEach((book) => titleArray.push(book.title));
+    return titleArray;
 };
 
 // Do not edit below this line
